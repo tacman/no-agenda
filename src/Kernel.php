4beb668794bc7a834a8df13eb159e7b0022824ae
Tac Tacelosky <tacman@gmail.com>
2025-05-07 08:19:55 -0400

    rector

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,23 +2,22 @@
 
 namespace App;
 
-use Minishlink\WebPush\WebPush;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
-class Kernel extends BaseKernel implements CompilerPassInterface
+class Kernel extends BaseKernel
 {
+
     use MicroKernelTrait;
 
-    public function process(ContainerBuilder $container): void
+    public function boot(): void
     {
-        $vapidPrivateKey = $_SERVER['VAPID_PRIVATE_KEY'] ?? null;
-        $vapidPublicKey = $_SERVER['VAPID_PUBLIC_KEY'] ?? null;
-
-        if (!$vapidPrivateKey || !$vapidPublicKey) {
-            $container->removeDefinition(WebPush::class);
+        static $started = false;
+        if ($started) {
+            assert(false);
         }
+        parent::boot();
     }
+
+
 }
